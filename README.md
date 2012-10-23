d63f7de07ac1ce343028afcfe9927c4bccd03864
Mikhail Davydov <azazel.private@gmail.com>
2012-10-24 03:01:50 +0600

    W3C Style Notifications API; $.notification() now returns $.Deferred

--- README.md
+++ README.md
@@ -1,69 +1,69 @@
-Webkit Notification API jQuery Wrapper
-======================================
+# Webkit Notification API jQuery Wrapper
 
   - It requests permission when it required (do not distrubs user on the start) you just call `$.notification` and this plugin will do the rest for you.
   - It abstracts 2 type of notifications (createHTMLNotification, createNotification) in one simple interface.
   - It allows you to specify auto-hide notification inteval (`timeout` parameter). 
   - It closes notification on click by default (`autoclose` parameter). Default browser action: user must click small close button on the top right corner to do that. 
   - You also can specify `replaceId` parameter to replace existed notification with new one (prevents from notifications flooding when showing chat messages in notifications, for eaxmple).
-  - You can pass `onclick`, `onclose`, `ondisplay`, `onerror` to watch Notification's events.
+  - You can pass `onclick`, `onclose`, `onshow`, `onerror` to watch Notification's events.
 
 This plug-in now works in Yandex.Mail WebChat (in production) and it does not have a strong dependence on jQuery.
 
-Usage
------
+## Usage
 
 ```javascript
-// Asynchronous usage. icon + text + title mode
+// icon + text + title
+
 var options = {
-    icon: 'avatar.png',
+    iconUrl: 'avatar.png',
     title: 'Title',
-    content: 'Message',
+    body: 'Message',
     onclick: function () {
         console.log('Pewpew');
     }
 };
 
-$.notification(options, function (isNotificationsAllowed, notification) {
-    if (isNotificationsAllowed) {
-        notification.show();
-    }
-});
-
-// Synchronous usage. "iframe" mode
-var options = {
-    url: 'http://ya.ru',
-    timeout: 10000, // close notification in 10 sec
-    autoclose: false,
-    onclick: function () {
-        console.log('Pewpew');
-    }
-};
-var notification = $.notification(options).show();
+$.notification(options);
 // notification will displayed as soon as user permit notifications
 ```
 
-Note
-----
+```javascript
+// just text
+$.notification("Message");
+// notification will displayed as soon as user permit notifications
+```
+
+```javascript
+// $.notification returns Deferred
+
+$.notification("Hello from jQuery.notifications!")
+.then(function (notification) {
+    setTimeout(function () {
+        notification.cancel();
+    }, 2000);
+}, function () {
+    console.error('Rejected!');
+});
+```
+
+## Note
 
 Before the notification will occur the ***user must click on the page*** and allow the notifications.
 But you can ask user beforehand:
 
 ```javascript
-$(document).one('click', function () {
-    window.webkitNotifications && window.webkitNotifications.requestPermission();
+$.notification.requestPermission(function () {
+    console.log($.notification.permissionLevel());
 });
 ```
 
-Read
-----
+## Read
 
 http://www.chromium.org/developers/design-documents/desktop-notifications/api-specification
 
 http://www.html5rocks.com/en/tutorials/notifications/quick/
 
-Licence
--------
+## Licence
 
 (The MIT License)
 
--- jquery.notification.js
+++ jquery.notification.js
@@ -10,204 +10,172 @@
 jQuery.notification = (function ($, window) {
 
 /**
+ * @see http://www.w3.org/TR/notifications/
+ *
+ * @type {Function}
+ */
+var W3CNotification = (function () {
+    // Chrome 22+ Safari 6+
+    if (window.Notification) return window.Notification;
+
+    var webkitNotifications = window.webkitNotifications;
+
+    // Non Webkit browsers
+    if (!webkitNotifications) return (function () {
+        var mockWebkitNotifications = {};
+        mockWebkitNotifications.permissionLevel = function () {
+            return "unsupported";
+        };
+        mockWebkitNotifications.requestPermission = $.noop;
+
+        return mockWebkitNotifications;
+    }());
+
+    // 1 undefined
+    // 2 forbidden
+    // 0 allowed
+    var notificationPermission = ["granted", "default", "denied"];
+
+    // Old WebKit browsers
+    var Notification = function (title, options) {
+        options = options || {};
+        if (!title) {
+            return;
+        }
+        var instance = webkitNotifications.createNotification(options.iconUrl || "", title, options.body || "");
+
+        instance.titleDir = options.titleDir || "auto";
+        instance.body = options.body || "";
+        instance.bodyDir = options.bodyDir || "auto";
+        instance.tag = options.tag || "";
+        instance.replaceId = options.tag || "";
+        instance.iconUrl = options.iconUrl || "";
+
+        instance.onclick = options.onclick || $.noop;
+        instance.onshow = options.onshow || $.noop;
+        instance.onerror = options.onerror || $.noop;
+        instance.onclose = options.onclose || $.noop;
+
+
+        if (Notification.permissionLevel() === "granted") {
+            instance.show();
+        }
+
+        return instance;
+    };
+
+    Notification.permissionLevel = function () {
+        return Notification.permission = notificationPermission[webkitNotifications.checkPermission()];
+    };
+
+    Notification.permissionLevel();
+
+    Notification.requestPermission = function (callback) {
+
+        if (Notification.permissionLevel() !== "default") {
+            callback();
+            return;
+        }
+
+
+        $(document).one('click', function () {
+            if (webkitNotifications.requestPermission.length) {
+                webkitNotifications.requestPermission(function () {
+                    Notification.permissionLevel();
+                    callback();
+                });
+                return;
+            }
+
+            // In old chrome webkitNotifications.requestPermission is without callback LOL
+            webkitNotifications.requestPermission();
+            var checkPermissionInterval = window.setInterval(function () {
+                var permissionLevel = Notification.permissionLevel();
+
+                if (permissionLevel !== "default") {
+                    window.clearInterval(checkPermissionInterval);
+                    callback();
+                }
+            }, 200);
+        });
+    };
+
+    return Notification;
+}());
+
+/**
  * Notification
  *
  * @constructor
  *
  * @param {Object}   options                  if url passed -> HTML page mode else icon & title & content mode
- * @param {String}   [options.url]
- * @param {String}   [options.icon]
+ * @param {String}   [options.iconUrl]
  * @param {String}   [options.title]
- * @param {String}   [options.content]
- * @param {String}   [options.replaceId]      acts as window.name in window.open
+ * @param {String}   [options.body]
+ * @param {String}   [options.tag]            acts as window.name in window.open
  * @param {Function} [options.onclick]
  * @param {Function} [options.onclose]
- * @param {Function} [options.ondisplay]
+ * @param {Function} [options.onshow]
  * @param {Function} [options.onerror]
  * @param {Boolean}  [options.autoclose=true] close block on click? close button is to small so its enable by default
  * @param {Number}   [options.timeout=Infinity] notification auto-close timeout
- * @param {Function} [callback]
  *
  * @example
- *      // Acync
- *      var options = {
- *          icon: 'avatar.png',
- *          title: 'Title',
- *          content: 'Message',
- *          onclick: function () {
- *              console.log('Pewpew');
- *          }
- *      };
- *      var message = $.notification(options, function (isNotificationsAllowed) {
- *          if (isNotificationsAllowed) {
- *              message.show();
- *          }
- *      });
  *
- *      // Synchronous
- *      var message = $.notification(options).show();
+ *      var message = $.notification(options);
  *      // messge will displayed as soon as user permit notifications
+ *
+ * @return {Deferred}
  */
-var Notification = function (options, callback) {
-    if (!(this instanceof Notification)) {
-        return new Notification(options, callback);
+var exports = function (options) {
+    var dfd = $.Deferred();
+
+    // Wired Safari 6 hack...
+    if (!W3CNotification.prototype) {
+        dfd.reject('unsupported');
+        return dfd.promise();
+    }
+
+    if (typeof options === "string") {
+        options = {
+            title: options
+        };
     }
     options = options || {};
     options.autoclose = typeof options.autoclose === "undefined" ? true : options.autoclose;
-    callback = callback || $.noop;
-    var self = this;
+    options.timeout = options.timeout || Infinity;
 
-    // is supported
-    if (!window.webkitNotifications) {
-        return callback(false, self);
-    }
-
-    // ask for permission
-    // 1 undefined
-    // 2 forbidden
-    // 0 allowed
-    this.notificationStatus = window.webkitNotifications.checkPermission();
-    this.requestPermission(function (isNotificationsAllowed) {
-        if (!isNotificationsAllowed) { // forbidden
-            return callback(isNotificationsAllowed, self);
-        }
-
-        // allowed
-        self.instance = options.url ?
-                        self.create(options.url) :
-                        self.create(options.icon, options.title, options.content);
-
-        options.onclick && self.instance.addEventListener('click', options.onclick, false);
-        if (options.autoclose) {
-            self.instance.addEventListener('click', function(){
-                self.cancel();
-            }, false);
-        }
-        options.onclose && self.instance.addEventListener('close', options.onclose, false);
-        options.ondisplay && self.instance.addEventListener('display', options.ondisplay, false);
-        options.onerror && self.instance.addEventListener('error', options.onerror, false);
-
-        self.instance.replaceId = options.replaceId || "";
-        self.timeout = options.timeout || Infinity;
-        // show already called
-        if (self.isShowCalled) {
-            self.show();
-        }
-        callback(isNotificationsAllowed, self);
-    });
-};
-
-Notification.queue = [];
-Notification.isAccessRequested = false;
-Notification.callQueue = function (isNotificationsAllowed) {
-    for (var i = 0, c = Notification.queue.length; i < c; i++) {
-        Notification.queue[i](isNotificationsAllowed);
-    }
-    Notification.queue = [];
-};
-
-Notification.prototype = {
-    constructor: Notification,
-
-    notificationStatus: 1,
-    isShowCalled: false,
-    timeout: Infinity,
-    instance: null,
-
-    /**
-     * displays notification
-     */
-    show: function () {
-        var self = this;
-        if (this.instance) {
-            this.instance.show();
-            if (isFinite(this.timeout)) {
-                window.setTimeout(function () {
-                    self.cancel();
-                }, this.timeout);
-            }
-        } else {
-            this.isShowCalled = true;
-        }
-
-        return this;
-    },
-
-    /**
-     * hides notification
-     */
-    cancel: function () {
-        if (this.instance) {
-            this.instance.cancel();
-        }
-
-        return this;
-    },
-
-    /**
-     * Creates instance
-     *
-     * @param {String} page_url_or_icon
-     * @param {String} [title]
-     * @param {String} [content]
-     */
-    create: function (page_url_or_icon, title, content) {
-        var instance;
-
-        // create instance
-        if (arguments.length === 1) {
-            if (window.webkitNotifications.createHTMLNotification) {
-                instance = window.webkitNotifications.createHTMLNotification(page_url_or_icon);
-            } else {
-                instance = window.webkitNotifications.createNotification('', '', page_url_or_icon);
-            }
-        } else {
-            instance = window.webkitNotifications.createNotification(page_url_or_icon, title, content);
-        }
-
-        return instance;
-    },
-
-    /**
-     * Acync wrapper over window.webkitNotifications.requestPermission()
-     *
-     * @param {Function} callback(isNotificationsAllowed)
-     */
-    requestPermission: function(callback) {
-        var self = this;
-
-        if (this.notificationStatus === 1) { // User deciding or not requested
-            Notification.queue.push(callback); // add callback to queue
-            if (Notification.isAccessRequested) { // already requested
-                return;
-            }
-        } else { // пользователь уже решил
-            callback(this.notificationStatus === 0);
+    W3CNotification.requestPermission(function () {
+        if ((W3CNotification.permission || W3CNotification.permissionLevel()) !== "granted") {
+            dfd.reject(W3CNotification.permissionLevel());
             return;
         }
 
-        Notification.isAccessRequested = true;
+        var instance = new W3CNotification(options.title, options);
 
-        var checkPermission = function () {
-            self.notificationStatus = window.webkitNotifications.checkPermission();
-            if (self.notificationStatus === 1) {
-                return; // still not decided
-            }
-            window.clearInterval(checkPermissionInterval);
-            Notification.callQueue(self.notificationStatus === 0); // 2 forbidden, 0 allowed
-        };
-
-        // requestPermission() must be called in user event listener!
-        $(document).one('click', function () {
-            window.webkitNotifications.requestPermission(checkPermission);
-        });
-
-        if (!window.webkitNotifications.requestPermission.length) {
-            var checkPermissionInterval = window.setInterval(checkPermission, 200);
+        if (isFinite(options.timeout)) {
+            instance.addEventListener('show', function () {
+                setTimeout(function () {
+                    instance.cancel();
+                }, options.timeout);
+            }, false);
         }
-    }
+
+        if (options.autoclose) {
+            instance.addEventListener('click', function () {
+                instance.cancel();
+            }, false)
+        }
+
+        dfd.resolve(instance);
+    });
+
+    return dfd.promise();
 };
 
-return Notification;
+exports.permissionLevel = W3CNotification.permissionLevel;
+exports.requestPermission = W3CNotification.requestPermission;
+
+return exports;
 
 }(jQuery, window));
